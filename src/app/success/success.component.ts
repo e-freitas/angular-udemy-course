b16a9d21b5a008c79597ef68d161d5bda072dec5
Elizeu Freitas <elizeu.freitas@outlook.com>
2019-07-09 22:38:40 -0300

    first commit, class 22

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-success',
+    template: `
+        <div class="container">
+            <div class="alert alert-success">
+                I'm the success component
+            </div>
+        </div>
+    `
+})
+export class SuccessComponent {
+
+}
